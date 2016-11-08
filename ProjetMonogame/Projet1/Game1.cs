bf0a02a0fea481dbfc47ab4c6ea8d3558b66504f
whitewolf1898 <chartier.andy@carrefour.cegepvicto.ca>
2016-11-08 16:52:23 -0500

    premier envoie github
    jeu monogame

---
+++ ProjetMonogame/Projet1/Game1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Projet1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        GameObject mario;
+        Rectangle fenetre;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            this.graphics.PreferredBackBufferWidth = graphics.GraphicsDevice.DisplayMode.Width;
+            this.graphics.PreferredBackBufferHeight = graphics.GraphicsDevice.DisplayMode.Height;
+            this.graphics.ToggleFullScreen();
+            fenetre = new Rectangle(0, 0, graphics.GraphicsDevice.DisplayMode.Width, graphics.GraphicsDevice.DisplayMode.Height);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            mario = new Projet1.GameObject();
+            mario.estVivant = true;
+            mario.position.X = 200;
+            mario.position.Y = 100;
+            mario.sprite = Content.Load<Texture2D>("st6.png");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if(Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                mario.position.X -= 4;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                mario.position.X += 4;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                mario.position.Y += 4;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                mario.position.Y -= 4;
+            }
+            if( mario.position.X < fenetre.Left)
+            {
+                mario.position.X = fenetre.Left;
+            }
+            if(mario.position.X > fenetre.Right - 150)
+            {
+                mario.position.X = fenetre.Right - 150;
+            }
+            if (mario.position.Y < fenetre.Top)
+            {
+                mario.position.Y = fenetre.Top;
+            }
+            if (mario.position.Y > fenetre.Bottom - 150)
+            {
+                mario.position.Y = fenetre.Bottom - 150;
+            }
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.Draw(mario.sprite,mario.position, Color.White);
+
+
+
+
+
+
+
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
